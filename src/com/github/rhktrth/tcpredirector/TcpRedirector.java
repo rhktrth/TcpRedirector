c9c94c402c70a9cc25f79d834fb183cefaebd270
rhktrth <46544833+rhktrth@users.noreply.github.com>
2019-01-24 21:00:36 +0900

    bufferedoutputstream

--- src/com/github/rhktrth/tcpredirector/TcpRedirector.java
+++ src/com/github/rhktrth/tcpredirector/TcpRedirector.java
@@ -6,9 +6,9 @@
 
 package com.github.rhktrth.tcpredirector;
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 
 public class TcpRedirector extends Thread {
@@ -26,33 +26,45 @@ public class TcpRedirector extends Thread {
 	}
 
 	private class DataTunnel extends Thread {
-		private final static int BUFFER_SIZE = 1000;
+		private final static int BUFFER_SIZE = 10000;
 
 		private InputStream inputStream;
-		private OutputStream outputStream;
-		private byte streamBuffer[] = new byte[BUFFER_SIZE];
+		private BufferedOutputStream outputStream;
+		private byte readBuffer[] = new byte[BUFFER_SIZE];
 
 		public DataTunnel(Socket inputSocket, Socket outputSocket) {
 			try {
 				inputStream = inputSocket.getInputStream();
-				outputStream = outputSocket.getOutputStream();
+				outputStream = new BufferedOutputStream(outputSocket.getOutputStream(), BUFFER_SIZE);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
 		public void run() {
+			int n = 0;
 			try {
-				int n;
-				while ((n = inputStream.read(streamBuffer)) > 0) {
-					outputStream.write(streamBuffer, 0, n);
-					outputStream.flush();
+				while ((n = inputStream.read(readBuffer)) > 0) {
+					outputStream.write(readBuffer, 0, n);
+					if (inputStream.available() == 0) {
+						outputStream.flush();
+					}
 				}
 			} catch (IOException e) {
 				// nothing to do
 			} finally {
 				try {
 					inputStream.close();
+				} catch (IOException e) {
+					// nothing to do
+				}
+
+				if (n < 0) {
+					n = 0;
+				}
+				try {
+					outputStream.write(readBuffer, 0, n);
+					outputStream.flush();
 					outputStream.close();
 				} catch (IOException e) {
 					// nothing to do
